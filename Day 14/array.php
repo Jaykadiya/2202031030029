f063bd6955a11f0eaefa52502529946524d308fc
Jaykadiya <137033371+Jaykadiya@users.noreply.github.com>
2023-07-17 09:32:36 -0800

    Add files via upload

---
+++ Day 14/array.php
@@ -0,0 +1,25 @@
+<?php
+echo "Indexed array:"."<br>";
+$arr = array (5,8,9,7,6,4,3);
+print_r ($arr );
+echo "<br>";
+
+echo "Associative Array:"."<br>";
+$jay = array ("name"=>"jay","subject"=>"Java","branch"=>"I.T.");
+print_r($jay );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
